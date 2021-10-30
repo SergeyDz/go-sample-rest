aaf3700c983410b180a035dd5d8b783f0923e93f
Sergey Dzyuban <sergey.dzyuban@gmail.com>
2021-10-30 20:22:29 +0000

    add docker push

--- .space.kts
+++ .space.kts
@@ -1,9 +1,19 @@
 job("Build and push Docker") {
     docker {
+        // get auth data from secrets and put it to env vars
+        env["DOCKERHUB_USER"] = Secrets("dockerhub_user")
+        env["DOCKERHUB_TOKEN"] = Secrets("dockerhub_token")
+
+        // put auth data to Docker config
+        beforeBuildScript {
+            content = """
+                B64_AUTH=${'$'}(echo -n ${'$'}DOCKERHUB_USER:${'$'}DOCKERHUB_TOKEN | base64)
+                echo "{\"auths\":{\"https://index.docker.io/v1/\":{\"auth\":\"${'$'}B64_AUTH\"}}}" > ${'$'}DOCKER_CONFIG/config.json
+            """
+        }
+
         build {
-            context = "docker"
-            file = "./Dockerfile"
-            labels["vendor"] = "mycompany"
+            labels["vendor"] = "tfg"
         }
 
         push("sergeydz/go-sample-rest") {
